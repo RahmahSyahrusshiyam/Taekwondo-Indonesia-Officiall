830ddb39b757f872b800746d2d76ffc713a22709
RahmahSyahrusshiyam <rsayhlu87@gmail.com>
2024-11-25 21:46:46 +0700

    menambahkan fie index.php

---
+++ indeks.php
@@ -0,0 +1,12 @@
+<!DOCYPE html>
+<htl lang="en">
+<head>
+    <meta charset="UTF-8>
+    <meta http-equiv="X-UA-compatible" content="IE=edge">
+    <meta name="viewport" content="width-device-width,initial-scale=1.0">
+    <title>sinkronisasi folder dengan github</title>
+</head>
+<body>
+    <h1>UTS Pengembangan Web</h1>
+</body>
+</html>
